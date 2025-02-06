3d4df8e7d8be449b96de7c93e3ab563097de1a52
Royal DSouza <roydza2725@gmail.com>
2025-02-06 06:25:55 +0530

    10th Video Completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
 
 
 function App() {
@@ -9,7 +10,9 @@ function App() {
     <>
     <Navbar title="TextUtils" aboutText="About-TextUtils"/>
     <div className="container my-3">
-      <TextForm heading="Enter the text to analyse"/>
+      {/* <TextForm heading="Enter the text to analyse"/> */}
+      {/* <About/> */}
+
     </div>
     </>
 
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,12 +3,27 @@ import React,{useState} from 'react'
 
 
 export default function TextForm(props) {
-  const [ text, setText ] = useState("Enter Text Here...");
+  const [ text, setText ] = useState("");
   const handleUpClick = () => {
     // console.log("Upercase Btn Was Clicked"+ text);
     let newText = text.toUpperCase();
     setText(newText);
   }
+
+  const handleLoClick = () => {
+    // console.log("Upercase Btn Was Clicked"+ text);
+    let newText = text.toLowerCase();
+    setText(newText);
+  }
+
+  const handleClearClick = () => {
+    // console.log("Upercase Btn Was Clicked"+ text);
+    let newText = ''
+    setText(newText);
+  }
+
+  
+
 
   const handleOnChange = (event) => {
     // console.log("On Change");
@@ -16,12 +31,23 @@ export default function TextForm(props) {
   }
 
   return (
-    <div>
-        <h1>{props.heading}</h1>
-        <div className="mb-3">
-            <textarea className="form-control" id="exampleFormControlTextarea1" value={text} onChange={handleOnChange} rows="8"></textarea>
-        </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
+    <>
+    <div className='container'>
+      <h1>{props.heading}</h1>
+      <div className="mb-3">
+        <textarea className="form-control" id="exampleFormControlTextarea1" value={text} onChange={handleOnChange} rows="8"></textarea>
+      </div>
+      <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to UpperCase</button>
+      <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to UpperCase</button>
+      <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
     </div>
+    <div className="container">
+      <h2>Your Summary : </h2>
+      <p>{text.split(" ").length} words and {text.length} Characters</p>
+      <p>{0.008 * text.split(" ").length} Minutes to Read</p>
+      <h2>Preview : </h2>
+      <p>{text}</p>
+    </div>
+    </>
   )
 }
