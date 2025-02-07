27a083e461bd839f80dbd30c0b81ff7bd994bb25
Royal DSouza <roydza2725@gmail.com>
2025-02-07 07:29:57 +0530

    TextUtils Completed

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import About from './components/About';
 import React,{useState} from 'react';
 import Alert from './components/Alert';
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Routes
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Link,
+//   Routes
+// } from "react-router-dom";
 
 
 function App() {
@@ -29,7 +29,18 @@ function App() {
     }, 1000);
   };
 
-  const toggleMode = () => {
+  const removeBodyClass = () => {
+    document.body.classList.remove('bg-light');
+    document.body.classList.remove('bg-dark');
+    document.body.classList.remove('bg-warning');
+    document.body.classList.remove('bg-danger');
+    document.body.classList.remove('bg-success');
+  }
+
+  const toggleMode = (cls) => {
+    removeBodyClass();
+    console.log(cls);
+    document.body.classList.add('bg-' + cls);
     if(mode === "light") {
       setMode("dark");
       document.body.style.backgroundColor = "#042743";
@@ -43,21 +54,21 @@ function App() {
 
   return (
     <>
-    <Router>
+    {/* <Router> */}
     <Navbar  title="TextUtils" mode={mode} toggleMode={toggleMode} aboutText="About-TextUtils"/>
     <Alert alert = {alert}/>
 
-    <Routes>
+    {/* <Routes>
           {/* <Route path="/" element={<Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} aboutText="About-TextUtils"/>}/> */}
-          <Route exact path="/TextForm" element={<TextForm />}/>
+          {/* <Route exact path="/TextForm" element={<TextForm />}/>
           <Route exa path="/about" element={<About />}/>
-    </Routes>
-    {/* <div className="container my-3">
-      <TextForm />
+    </Routes>} */}
+    <div className="container my-3">
+    <TextForm  title="Use TextUtils : " mode={mode} showAlert= {showAlert}/>
       
 
-    </div> */}
-    </Router>
+    </div>
+    {/* </Router> */}
     </>
 
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,17 +7,17 @@ export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-lg bg-${props.mode} navbar-${props.mode}`}>
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">{props.title}</Link>
+          <a className="navbar-brand" href="#">{props.title}</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/textForm">Home</Link>
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
               </li>
               <li className="nav-item">
-            <Link className="nav-link" to="/about">{props.aboutText}</Link>
+            <a className="nav-link" href="#">{props.aboutText}</a>
               </li>
             </ul>
 
@@ -46,10 +46,17 @@ export default function Navbar(props) {
 
             </div> */}
 
+            <div className="d-flex">
+                <div className="bg-primary rounded mx-2" onClick={() => {props.toggleMode('primary')}} style={{height:'30px',width:'30px'}}></div>
+                <div className="bg-danger rounded mx-2" onClick={() => {props.toggleMode('danger')}} style={{height:'30px',width:'30px'}}></div>
+                <div className="bg-success rounded mx-2" onClick={() => {props.toggleMode('success')}} style={{height:'30px',width:'30px'}}></div>
+                <div className="bg-warning rounded mx-2" onClick={() => {props.toggleMode('warning')}} style={{height:'30px',width:'30px'}}></div>
+            </div>
+
 
             <div className={`form-check form-switch text-${props.mode==='light'?'#042743':'light'}`}>
-              <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
-              <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable {props.mode==='light'?'dark':'light'} Mode</label>
+              <input className="form-check-input" onClick={() => {props.toggleMode(null)}} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+              <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Toggle Mode</label>
             </div>
 
             {/* <form className="d-flex" role="search">
